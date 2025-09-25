56fa401cbc55c1836e205be894225bdaec96ac97
evgeniy <uhvatkin.e@gigwork.ru>
2025-09-25 20:23:25 +0300

    правки

--- public/index.php
+++ public/index.php
@@ -105,8 +105,10 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, array $args) 
     }
     $document = new Document($checkedUrl, true);
     if ($document->has('h1')) {
-        $h1 = $document->find('h1');
-        $check['h1'] = $h1[0]->text();
+        $h1 = $document->find('h1') ?? null;
+        if($h1 instanceof \DiDom\Element) {
+            $check['h1'] = $h1[0]->text();
+        }
     }
     if ($document->has('title')) {
         $title = $document->find('title');
